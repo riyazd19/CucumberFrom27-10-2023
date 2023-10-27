e74490e014cfa3f80ebbf6d400774d2da1a4b945
Khan_Pc <Khan_Pc@DESKTOP-C27HO6T>
2023-10-27 17:57:37 +0530

    first commit

---
+++ cucumberFrom27102023/src/test/java/cucumberFrom27102023/cucumberFrom27102023/AppTest.java
@@ -0,0 +1,20 @@
+package cucumberFrom27102023.cucumberFrom27102023;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
