96b39d43554826885cddf0c539bf37f1b2bea4a4
Khan_Pc <Khan_Pc@DESKTOP-C27HO6T>
2023-10-27 23:12:37 +0530

    adding plugins in runner class

--- cucumberFrom27102023/src/test/java/stepDefinition/TestRunner.java
+++ cucumberFrom27102023/src/test/java/stepDefinition/TestRunner.java
@@ -7,7 +7,9 @@ import io.cucumber.junit.CucumberOptions;
 
 //@RunWith(Cucumber.class)
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/features",glue= {"StepDefinition"})
+@CucumberOptions(features="src/test/resources/features",glue= {"stepDefinition"},
+monochrome=true,
+plugin={"pretty","html:/target/HtmlReports"})
 public class TestRunner {
 
 }
