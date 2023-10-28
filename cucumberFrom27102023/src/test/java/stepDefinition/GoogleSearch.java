ae6e9afe6c2b23e2355e44a0457ecf65462f07d7
Khan_Pc <Khan_Pc@DESKTOP-C27HO6T>
2023-10-28 14:57:46 +0530

    complete google search prog with selenium

--- cucumberFrom27102023/src/test/java/stepDefinition/GoogleSearch.java
+++ cucumberFrom27102023/src/test/java/stepDefinition/GoogleSearch.java
@@ -1,7 +1,13 @@
 package stepDefinition;
 
-import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -12,29 +18,38 @@ public class GoogleSearch {
 
 	@Given("User fires up the laptop and opens the browser")
 	public void user_fires_up_the_laptop_and_opens_the_browser() {
-
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
-		driver.get("http//:www.google.com");
-		throw new io.cucumber.java.PendingException();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		System.out.println("till here prog is executing");
+		driver.get("https://www.google.com");
+
 	}
 
 	@When("user first search for google")
-	public void user_first_search_for_google() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+	public void user_first_search_for_google() throws InterruptedException {
+		Thread.sleep(4000);
+		// Actions action= new Actions(driver);
+		// action.
+		driver.findElement(By.id("APjFqb")).click();
+		driver.findElement(By.id("APjFqb")).sendKeys("Automation Step by step");
+
 	}
 
-	@When("user in the search bar enters some value to search for")
+	@And("user in the search bar enters some value to search for")
 	public void user_in_the_search_bar_enters_some_value_to_search_for() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+		System.out.println("no hitting the enter");
+		driver.findElement(By.id("APjFqb")).sendKeys(Keys.ENTER);
+
 	}
 
 	@Then("user is able to see the results")
 	public void user_is_able_to_see_the_results() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+		System.out.println("now verifying if user is able to see the results");
+		driver.getPageSource().contains("Online Cources");
+
 	}
 
 }
--- cucumberFrom27102023/src/test/resources/features/googleSearch.feature
+++ cucumberFrom27102023/src/test/resources/features/googleSearch.feature
@@ -1,6 +1,8 @@
 Feature: search in google
 
+  @Riya
   Scenario: to search in google
+  
     Given User fires up the laptop and opens the browser
     When user first search for google
     And user in the search bar enters some value to search for
